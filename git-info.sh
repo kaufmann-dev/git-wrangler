ed8c078a104fd8ed2a7cbd3a1b815e9e588594a1
David Kaufmann <david@kaufmann.dev>
2024-06-02 17:51:06 +0100

    refactor(git-info): translate output to english and use printf

--- git-info.sh
+++ git-info.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "------------------------------------------------------------------"
+printf -- "------------------------------------------------------------------\n"
 
 find . -maxdepth 2 -type d -name '.git' | while read git_dir; do
     repo_name=$(dirname "$git_dir")
@@ -8,26 +8,26 @@ find . -maxdepth 2 -type d -name '.git' | while read git_dir; do
     cd "$repo_name" || exit
 
     if [ "$repo_name" = "." ]; then
-        echo -e "Repository:            \e[1;32m"${PWD##*/}"\e[0m"
+        printf "Repository:            \e[1;32m"${PWD##*/}"\e[0m\n"
     else
-        echo -e "Repository:            \e[1;32m$(basename "$repo_name")\e[0m"
+        printf "Repository:            \e[1;32m$(basename "$repo_name")\e[0m\n"
     fi
 
-    branch_count=$(git branch -a | grep -v 'remotes' | wc -l)
-    echo "Anzahl der Branches:   $branch_count"
+    branch_count=$(git branch -a | grep -v 'remotes' | wc -l | tr -d '[:space:]')
+    printf "Anzahl der Branches:   $branch_count\n"
 
-    echo -n "Branches:              "
+    printf "Branches:              "
     # git branch -a | grep -v 'remotes' | sed -n 's/^\* //p; 2,$s/^/                     /p'
     git branch -a | grep -v 'remotes' | awk '{sub(/^\* /, ""); if (NR==1) print $0; else printf "%-21s%s\n", "", $0}'
 
-    echo -n "Autoren und Committer: "
+    printf "Autoren und Committer: "
     # git log --format='%an <%ae>' | sort -u | sed '1!s/^/ /' | sed '2,$s/^/                      /'
     git log --format='%an <%ae>' | sort -u | awk 'NR==1{print $0} NR>1{printf "%-23s%s\n", "", $0}'
 
     commit_count=$(git rev-list --all --count)
-    echo "Gesamtanzahl Commits:  $commit_count"
+    printf "Gesamtanzahl Commits:  $commit_count\n"
 
     cd .. || exit
 
-    echo "------------------------------------------------------------------"
+    printf -- "------------------------------------------------------------------\n"
 done
