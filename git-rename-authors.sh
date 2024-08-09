8b146de01c42e73ab94c6e1f78c283c89b40fea8
David Kaufmann <david@kaufmann.dev>
2024-08-09 12:16:41 +0100

    refactor(git-rename-authors): improve output and use subshell

--- git-rename-authors.sh
+++ git-rename-authors.sh
@@ -11,31 +11,29 @@ while [[ $# -gt 0 ]]; do
             shift 2
             ;;
         *)
-            echo "Unknown option: $1"
+            printf "\e[31mUnknown option: $1\e[0m\n"
             exit 1
             ;;
     esac
 done
 
 if [[ -z "$NEW_NAME" || -z "$NEW_EMAIL" ]]; then
-    echo "Error: Both --name and --email options must be provided."
+    printf "\e[31mError: Both --name and --email options must be provided.\e[0m\n"
     exit 1
 fi
 
 repos=$(find . -mindepth 1 -maxdepth 2 -type d -name ".git")
 
 for repo in $repos; do
-    echo "Updating repository: $repo"
+    (
+        printf "\e[32mAuthor and commiter information updated for $repo\e[0m\n"
 
-    repo_dir=$(dirname "$repo")
-    cd "$repo_dir" || exit
+        repo_dir=$(dirname "$repo")
+        cd "$repo_dir" || exit
 
-    git filter-repo --partial --email-callback "return b'$NEW_EMAIL'" \
-                    --name-callback "return b'$NEW_NAME'"
+        git filter-repo --partial --email-callback "return b'$NEW_EMAIL'" \
+                        --name-callback "return b'$NEW_NAME'"
 
-    git push --force
-
-    cd - || exit
+        cd - || exit
+    )
 done
-
-echo "Author and committer information updated for all repositories."
