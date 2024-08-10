1a9cfc4237f5c5cdac1c691b0d0d3a0755385e83
David Kaufmann <david@kaufmann.dev>
2024-08-10 10:26:40 +0100

    feat(git-rename-authors): add --force option and error handling

--- git-rename-authors.sh
+++ git-rename-authors.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+force=false
+
 while [[ $# -gt 0 ]]; do
     case "$1" in
         --name)
@@ -9,6 +11,10 @@ while [[ $# -gt 0 ]]; do
         --email)
             NEW_EMAIL="$2"
             shift 2
+            ;;
+        --force)
+            force=true
+            shift
             ;;
         *)
             printf "\e[31mUnknown option: $1\e[0m\n"
@@ -22,18 +28,35 @@ if [[ -z "$NEW_NAME" || -z "$NEW_EMAIL" ]]; then
     exit 1
 fi
 
-repos=$(find . -mindepth 1 -maxdepth 2 -type d -name ".git")
+repos=$(find . -maxdepth 2 -type d -name ".git")
 
 for repo in $repos; do
     (
-        printf "\e[32mAuthor and commiter information updated for $repo\e[0m\n"
-
         repo_dir=$(dirname "$repo")
+
+        if [ "$repo_dir" = "." ]; then
+            repo_name_display="${PWD##*/}"
+        else
+            repo_name_display=$(basename "$repo_dir")
+        fi
+
         cd "$repo_dir" || exit
 
-        git filter-repo --partial --email-callback "return b'$NEW_EMAIL'" \
-                        --name-callback "return b'$NEW_NAME'"
+        if [ "$force" = true ]; then
+            error_message=$(git filter-repo --partial --force --email-callback "return b'$NEW_EMAIL'" \
+                            --name-callback "return b'$NEW_NAME'" 2>&1 >/dev/null)
+        else
+            error_message=$(git filter-repo --partial --email-callback "return b'$NEW_EMAIL'" \
+                            --name-callback "return b'$NEW_NAME'" 2>&1 >/dev/null)
+        fi
 
-        cd - || exit
+        if [ $? -ne 0 ]; then
+            printf "\e[31mError: Could not update git author and commiter information for $repo_name_display: $(echo "$error_message" | tr '\n' ' ' | sed 's/ \{2,\}/ /g')\e[0m\n"
+            continue
+        fi
+
+        printf "\e[32mAuthor and commiter information updated for $repo_name_display\e[0m\n"
+
+        cd .. || exit
     )
 done
