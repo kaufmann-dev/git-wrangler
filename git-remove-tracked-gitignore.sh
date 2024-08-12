d9664314ac4b59ac852e712c104327480221330c
David Kaufmann <david@kaufmann.dev>
2024-08-12 13:27:26 +0100

    refactor(scripts): add argument parsing and subshell to git-info and git-remove-tracked-gitignore

--- git-remove-tracked-gitignore.sh
+++ git-remove-tracked-gitignore.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+COMMIT=false
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --commit)
+            COMMIT=true
+            shift
+            ;;
+        *)
+            printf "\e[31mUnknown option: $1\e[0m\n"
+            exit 1
+            ;;
+    esac
+done
+
 find . -maxdepth 2 -type d -name '.git' | while read -r git_dir; do
     (
         repo_dir=$(dirname "$git_dir")
@@ -16,8 +31,12 @@ find . -maxdepth 2 -type d -name '.git' | while read -r git_dir; do
             ignored_files=$(git ls-files --ignored --exclude-standard -o -z)
             if [ -n "$ignored_files" ]; then
                 git rm --cached -z $ignored_files
-                git commit -m "Stop tracking files defined in .gitignore" -q
-                printf "\e[32mStopped tracking files defined in .gitignore for $repo_name_display\e[0m\n"
+                if [ "$COMMIT" = true ]; then
+                    git commit -m "Stop tracking files defined in .gitignore" -q
+                    printf "\e[32mStopped tracking files defined in .gitignore for $repo_name_display. Commit performed.\e[0m\n"
+                else
+                    printf "\e[32mStopped tracking files defined in .gitignore for $repo_name_display. (No commit performed)\e[0m\n"
+                fi
             else
                 printf "\e[33mNo files tracked that were defined in .gitignore for $repo_name_display. Skipping...\e[0m\n"
             fi
