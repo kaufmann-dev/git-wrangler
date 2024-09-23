9126620fb39f9c0a5cf8e6ca5dee3a5881ced964
David Kaufmann <david@kaufmann.dev>
2024-09-23 18:10:04 +0100

    refactor: improve git-rename-branches.sh with color output and subshell loop

--- git-rename-branches.sh
+++ git-rename-branches.sh
@@ -11,35 +11,37 @@ while [[ $# -gt 0 ]]; do
             shift 2
             ;;
         *)
-            echo "Unknown option: $1"
+            printf "\e[31mUnknown option: $1\e[0m\n"
             exit 1
             ;;
     esac
 done
 
 if [ -z "${oldbranch+x}" ] || [ -z "${newbranch+x}" ]; then
-    echo "Error: Both --oldbranch and --newbranch options must be provided."
-    echo "Usage: $0 --oldbranch <old_branch_name> --newbranch <new_branch_name>"
+    printf "\e[31mError: Both --oldbranch and --newbranch options must be provided.\e[0m\n"
+    printf "Usage: $0 --oldbranch <old_branch_name> --newbranch <new_branch_name>\n"
     exit 1
 fi
 
 find . -maxdepth 2 -type d -name '.git' | while read git_dir; do
-    repo_name=$(dirname "$git_dir")
+    (
+        repo_name=$(dirname "$git_dir")
 
-    if [ "$repo_name" = "." ]; then
-        repo_name_display="${PWD##*/}"
-    else
-        repo_name_display=$(basename "$repo_name")
-    fi
+        if [ "$repo_name" = "." ]; then
+            repo_name_display="${PWD##*/}"
+        else
+            repo_name_display=$(basename "$repo_name")
+        fi
 
-    cd "$repo_name" || exit
+        cd "$repo_name" || exit
 
-    if git rev-parse --verify --quiet "refs/heads/$oldbranch" > /dev/null; then
-        git branch -m "$oldbranch" "$newbranch"
-        echo -e "\e[32mBranch $oldbranch im Repository $repo_name_display umbenannt zu $newbranch.\e[0m"
-    else
-        echo -e "\e[33mIm Repository $repo_name_display existiert kein Branch $oldbranch.\e[0m"
-    fi
+        if git rev-parse --verify --quiet "refs/heads/$oldbranch" > /dev/null; then
+            git branch -m "$oldbranch" "$newbranch"
+            printf "\e[32mBranch $oldbranch im Repository $repo_name_display umbenannt zu $newbranch.\e[0m\n"
+        else
+            printf "\e[33mIm Repository $repo_name_display existiert kein Branch $oldbranch.\e[0m\n"
+        fi
 
-    cd .. || exit
+        cd .. || exit
+    )
 done
