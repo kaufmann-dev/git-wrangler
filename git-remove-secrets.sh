2f77477d5b00f67fbc9cd259271c6c5dbcc3abd2
David Kaufmann <david@kaufmann.dev>
2024-10-31 17:36:19 +0100

    feat: implement git-remove-secrets.sh script

--- git-remove-secrets.sh
+++ git-remove-secrets.sh
@@ -1,1 +1,67 @@
-# tbd
+#!/bin/bash
+
+default_secrets=(
+    "appsettings.json"
+    ".env"
+)
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --secrets)
+            shift
+            secrets_file="$1"
+            ;;
+        *)
+            printf "\e[31mUnknown option: $1\e[0m\n"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+if ! command -v git &> /dev/null; then
+    printf "\e[31mError: 'git' is not installed. Please install it first.\e[0m\n"
+    exit 1
+fi
+
+if ! command -v git-filter-repo &> /dev/null; then
+    printf "\e[31mError: 'git filter-repo' is not installed. Please install it first.\e[0m\n"
+    exit 1
+fi
+
+if [ -n "$secrets_file" ]; then
+    if [ -e "$secrets_file" ]; then
+        IFS=$'\n' read -d '' -r -a default_secrets < "$secrets_file"
+    else
+        printf "Specified secrets file not found: $secrets_file\n"
+        exit 1
+    fi
+fi
+
+for repo in $(find . -maxdepth 2 -type d -name '.git'); do
+    repo_path=$(dirname "$repo")
+
+    if [ "$repo_dir" = "." ]; then
+        repo_name_display="${PWD##*/}"
+    else
+        repo_name_display=$(basename "$repo_dir")
+    fi
+    
+    secret_files=""
+    for secret in "${default_secrets[@]}"; do
+        secret_files+=" -o -name '$secret'"
+    done
+    secret_files=${secret_files:3}  # Remove leading " -o"
+
+    found_secret_files=$(find "$repo_path" -type f \( $secret_files \))
+    
+    if [ -n "$found_secret_files" ]; then
+        if git -C "$repo_path" filter-repo --path $found_secret_files --invert-paths; then
+            printf "\e[32mSecret files successfully removed from $repo_name_display\e[0m\n"
+        else
+            printf "\e[31mError: Unable to remove secret files from $repo_name_display\e[0m\n"
+        fi
+    else
+        printf "\e[33mNo secret files found in $repo_name_display. Skipping...\e[0m\n"
+    fi
+done
