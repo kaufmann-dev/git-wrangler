d01b9b37e8c0f2ad0c8804e986080680a49f8102
David Kaufmann <david@kaufmann.dev>
2024-12-23 17:24:45 +0100

    feat: add --force option and improve error handling

--- git-rename-commits.sh
+++ git-rename-commits.sh
@@ -2,6 +2,7 @@
 
 min_msg_length=5
 standard_msg="Commit changes"
+force=false
 
 while [[ $# -gt 0 ]]; do
   case "$1" in
@@ -16,6 +17,10 @@ while [[ $# -gt 0 ]]; do
     --standardmsg)
       standard_msg=$2
       shift 2
+      ;;
+    --force)
+      force=true
+      shift
       ;;
     *)
       printf "\e[31mUnknown option: $1\e[0m\n"
@@ -50,6 +55,8 @@ find . -maxdepth 2 -type d -name '.git' | while read -r git_dir; do
 
     cd "$repo_path" || exit
 
+    commit_changed_flag=false
+
     git log --pretty=format:%H --reverse | while read -r commit_hash; do
       file_count=$(git log --pretty=format:%n --name-status -1 "$commit_hash" | grep -E '^[ADRM]\s+[^[:space:]]+$' | wc -l)
       old_message=$(git log --pretty=format:%B -1 "$commit_hash")
@@ -57,36 +64,61 @@ find . -maxdepth 2 -type d -name '.git' | while read -r git_dir; do
 
       if ((message_length <= min_msg_length)) || [[ " ${messages[@]} " =~ " $old_message " ]]; then
         if ((file_count == 1)); then
-          file_status=$(git log --pretty=format:%n --name-status -1 "$commit_hash" | awk '{print $1}')
-          file_name=$(git log --pretty=format:%n --name-status -1 "$commit_hash" | awk '{print $2}')
+            file_status=$(git log --pretty=format:%n --name-status -1 "$commit_hash" | awk '{print $1}')
+            file_name=$(git log --pretty=format:%n --name-status -1 "$commit_hash" | awk '{print $2}')
 
-          case "$file_status" in
-            'A')
-              new_message="Added $file_name"
-              ;;
-            'D')
-              new_message="Removed $file_name"
-              ;;
-            'M')
-              new_message="Changed $file_name"
-              ;;
-            *)
-              new_message="$standard_msg"
-              ;;
-          esac
+            case "$file_status" in
+                'A')
+                    new_message="Added $file_name"
+                    ;;
+                'D')
+                    new_message="Removed $file_name"
+                    ;;
+                'M')
+                    new_message="Changed $file_name"
+                    ;;
+                *)
+                    new_message="$standard_msg"
+                    ;;
+            esac
 
-          git filter-repo --partial --message-callback 'return message.decode("utf-8").replace("'"$old_message"'", "'"$new_message"'").encode("utf-8")' --force > /dev/null 2>&1
+            if [ "$force" = true ]; then
+                error_message=$(git filter-repo --partial --message-callback 'return message.decode("utf-8").replace("'"$old_message"'", "'"$new_message"'").encode("utf-8")' --force 2>&1 >/dev/null)
+            else
+                error_message=$(git filter-repo --partial --message-callback 'return message.decode("utf-8").replace("'"$old_message"'", "'"$new_message"'").encode("utf-8")' 2>&1 >/dev/null)
+            fi
 
-          printf "Commit message of commit $commit_hash changed from '$old_message' to '$new_message'\n"
+            if [ $? -ne 0 ]; then
+                printf "\e[31mError: Could not update commit message for $commit_hash in $repo_name_display: $(echo "$error_message" | tr '\n' ' ' | sed 's/ \{2,\}/ /g')\e[0m\n"
+                continue
+            fi
+
+            printf "Commit message of commit $commit_hash changed from '$old_message' to '$new_message'\n"
+            commit_changed_flag=true
         else
-          git filter-repo --partial --message-callback 'return message.decode("utf-8").replace("'"$old_message"'", "'"$standard_msg"'").encode("utf-8")' --force > /dev/null 2>&1
+            if [ "$force" = true ]; then
+                error_message=$(git filter-repo --partial --message-callback 'return message.decode("utf-8").replace("'"$old_message"'", "'"$standard_msg"'").encode("utf-8")' --force 2>&1 >/dev/null)
+            else
+                error_message=$(git filter-repo --partial --message-callback 'return message.decode("utf-8").replace("'"$old_message"'", "'"$standard_msg"'").encode("utf-8")' 2>&1 >/dev/null)
+            fi
 
-          printf "Commit message of commit $commit_hash changed from '$old_message' to '$standard_msg'\n"
+            if [ $? -ne 0 ]; then
+                printf "\e[31mError: Could not update commit message for $commit_hash in $repo_name_display: $(echo "$error_message" | tr '\n' ' ' | sed 's/ \{2,\}/ /g')\e[0m\n"
+                continue
+            fi
+
+            printf "Commit message of commit $commit_hash changed from '$old_message' to '$standard_msg'\n"
+            commit_changed_flag=true
         fi
-      fi
+    fi
     done
 
-    printf "\e[32mChanged commit messages for $repo_name_display successfully.\e[0m\n"
+    if $commit_changed_flag; then
+      printf "\e[32mChanged commit messages for $repo_name_display successfully.\e[0m\n"
+    else
+      printf "\e[33mNo comit messages changed in $repo_name_display. Skipping...\e[0m\n"
+    fi
+
     printf ""
   )
 done
