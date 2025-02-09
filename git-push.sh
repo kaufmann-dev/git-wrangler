92509786bacbeaf43ee5061d4ebc9c36cadbed78
David Kaufmann <david@kaufmann.dev>
2025-02-09 17:44:08 +0100

    fix(git-push): improve push output capture and directory navigation

--- git-push.sh
+++ git-push.sh
@@ -40,21 +40,27 @@ echo "$git_repositories" | while read git_dir; do
         cd "$repo_path" || exit
 
         if [ "$force" = true ]; then
-            push_output=$(git push --force origin HEAD 2>&1)
-        else
-            push_output=$(git push origin HEAD 2>&1)
-        fi
-
-        push_output=$(echo "$push_output" | tr '\n' ' ' | sed 's/ \{2,\}/ /g')
-
-        if [ "$?" -eq 0 ]; then
-            if ! printf "$push_output" | grep -q "Everything up-to-date"; then
-                printf "\e[32mGit push completed for $repo_name_display\e[0m\n"
+            if push_output=$(git push --force origin HEAD 2>&1); then
+                if ! printf "$push_output" | grep -q "Everything up-to-date"; then
+                    printf "\e[32mGit push completed for $repo_name_display\e[0m\n"
+                else
+                    printf "\e[33mNo changes to push for $repo_name_display. Skipping...\e[0m\n"
+                fi
             else
-                printf "\e[33mNo changes to push for $repo_name_display. Skipping...\e[0m\n"
+                printf "\e[31mGit push failed for $repo_name_display:\n$push_output\e[0m\n"
             fi
         else
-            printf "\e[31mGit push failed for $repo_name_display: $push_output\e[0m\n"
+            if push_output=$(git push origin HEAD 2>&1); then
+                if ! printf "$push_output" | grep -q "Everything up-to-date"; then
+                    printf "\e[32mGit push completed for $repo_name_display\e[0m\n"
+                else
+                    printf "\e[33mNo changes to push for $repo_name_display. Skipping...\e[0m\n"
+                fi
+            else
+                printf "\e[31mGit push failed for $repo_name_display:\n$push_output\e[0m\n"
+            fi
         fi
+    
+        cd .. || exit
     )
 done
