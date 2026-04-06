2fe16c3782f366ad65614922dec0bb0369908e77
David Kaufmann <david@kaufmann.dev>
2026-04-06 09:28:38 +0100

    fix: preserve original working directory when using wrangler.cmd

--- wrangler.cmd
+++ wrangler.cmd
@@ -1,4 +1,6 @@
 @echo off
+set "WRANGLER_INVOCATION_DIR=%CD%"
+if defined WSLENV (set "WSLENV=WRANGLER_INVOCATION_DIR/p:%WSLENV%") else (set "WSLENV=WRANGLER_INVOCATION_DIR/p")
 pushd "%~dp0"
 bash wrangler %*
 popd
