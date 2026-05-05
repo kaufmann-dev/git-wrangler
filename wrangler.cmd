c5ebd293ea812b39edf0e8b71af77f9d10a4f2ee
David Kaufmann <david@kaufmann.dev>
2026-05-05 17:38:18 +0200

    chore: remove Windows cmd wrapper and invocation dir handling

--- wrangler.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-set "WRANGLER_INVOCATION_DIR=%CD%"
-if defined WSLENV (set "WSLENV=WRANGLER_INVOCATION_DIR/p:%WSLENV%") else (set "WSLENV=WRANGLER_INVOCATION_DIR/p")
-pushd "%~dp0"
-bash wrangler %*
-popd
